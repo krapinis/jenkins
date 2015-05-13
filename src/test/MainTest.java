e64feccbe8a23d6c118a5c2c6a330fd076252e28
krapinis <mantas1zinkevicius@gmail.com>
2015-05-13 13:52:22 +0200

    initial commit

---
+++ src/test/MainTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void test() {
+		Object object = null;
+		
+		assertNull(object);
+	}
+
+}
